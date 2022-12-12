686a0cfde79800187c92dc7905f88a51a559db39
samuel <samuel.bestofall.engineer@gmail.com>
2022-12-12 23:05:36 +0900

    Add funtional interface(Ex14_2) and predicate(Ex14_3)

---
+++ java-jungsuk-basic/src/main/java/app/Ex14_3.java
@@ -0,0 +1,24 @@
+package main.java.app;
+
+import java.util.function.Predicate;
+
+public class Ex14_3 {
+    public static void main(String[] args) {
+        Predicate<Integer> p = i -> i < 100;
+        Predicate<Integer> q = i -> i < 200;
+        Predicate<Integer> r = i -> i % 2 == 0;
+        Predicate<Integer> notP = p.negate();
+        Predicate<Integer> notQ = q.negate();
+        Predicate<Integer> notR = r.negate();
+
+        Predicate<Integer> all = notP.and(q.or(r));
+        System.out.println(all.test(150));
+
+        String str1 = "abc";
+        String str2 = "abc";
+
+        Predicate<String> p2 = Predicate.isEqual(str1);
+        boolean result = p2.test(str2);
+        System.out.println(result);
+    }
+}
